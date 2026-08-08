08c26607b5f8bc7a9770cf92f166929dec848a04
Andrew Balabanov <andrewbalabanov13@gmail.com>
2026-08-08 11:47:11 -0700

    added a ratelimit to prevent dos

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 import socket
 import threading
-
+import time
 COMPUTER_IP = "172.16.0.0"
 PUBLIC_IP = "192.168.1.133"
 HOST = "25.0.141.101"
 PORT = 443
+
 
 def thread_handle(conn,address,connection_lock,connections):
     try:
@@ -27,6 +28,7 @@ def thread_handle(conn,address,connection_lock,connections):
 
 def main():
     connections = {}
+    previous_connection = {}
     connection_lock = threading.Lock()
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
@@ -36,17 +38,26 @@ def main():
         print("listening")
 
         while True:
+            deny_connection = False
             conn, address = server.accept()
-
-            print(f"accepted client, adress: {address}")
-
-            
-
-            thread = threading.Thread(target=thread_handle,args=(conn,address,connection_lock,connections),daemon=True)
             with connection_lock:
-                connections[address]=conn
+                for other_adress, other_con in connections.items():
+                    if other_adress[0] == address[0]:
+                        deny_connection = True
+            client_ip = address[0]
 
-            thread.start()
+            if client_ip in previous_connection:
+                if time.time() - previous_connection[client_ip] < 10:
+                    deny_connection = True
+
+            if not deny_connection:
+                previous_connection[address[0]]=time.time()
+                thread = threading.Thread(target=thread_handle,args=(conn,address,connection_lock,connections),daemon=True)
+                with connection_lock:
+                    connections[address]=conn
+                print(f"accepted client, adress: {address}")
+
+                thread.start()
 
 
 main()
